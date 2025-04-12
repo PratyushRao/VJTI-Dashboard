78f45f0e83358b6225bc71f86f97c8d5092ff498
PratyushRao <pratyur06@gmail.com>
2025-04-12 15:05:43 +0530

    Added Routing to faculty page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import {Routes, Route, Navigate} from 'react-router-dom';
 import Login from './login/login.tsx';
 import Student from './student-section/student.tsx';
+import Faculty from './faculty-section/faculty.tsx';
 import './index.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Route path="*" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/student" element={<Student />} />
+        <Route path="/faculty" element={<Faculty />} />
         {/*add other routes here*/}
       </Routes>
   );
